1e215a719208d58cace9370bcc5cb045b840788a
chetansankpal7007@gmail.com <chetansankpal7007@gmail.com>
2023-06-30 08:47:39 +0530

    jun_30_create_component_props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App_A.css";
+import Navbar from './component/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Chetan
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="React Class -2"/>
+    </>
   );
 }
 
