40ee246823eccdfd0afcb5c11afa0b25ed813014
chetansankpal7007@gmail.com <chetansankpal7007@gmail.com>
2023-07-05 08:46:57 +0530

    event-useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App_A.css";
 import Navbar from './component/Navbar'
-import Newcompoent from './component/New_compoent'
-const link= "https://react.dev/";
 function App() {
   return (
     <>
-      <Navbar title="React Class -2"  reactLink={link}/>
-      <Navbar title="React Class -2"  reactLink={link}/>
-      <Navbar title="React Class -2"  reactLink={link}/>
-      <Newcompoent/>
+      <Navbar title="Events" />
     </>
   );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,53 +1,31 @@
-import React from 'react'
-import Search from './Search'
-import PropTypes from 'prop-types'
+import React, {useState} from 'react'
 
 export default function Navbar(props) {
+
+  const [text, setText] = useState('demo text');
+
+  const clickMe = () => {
+    console.log("click function call");
+    console.log(text);
+    setText("text change")
+  }
+
+  const handelOnChange = (events) => {
+    console.log(events.target.value);
+    setText(events.target.value);
+  }
+
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          {props.title}
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    <div className='contener m-5'>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">
-                {props.home} <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href={props.reactLink}>
-                Link
-              </a>
-            </li>
-          </ul>
-          <Search></Search>
-        </div>
-      </nav>
+        <textarea value={text} onChange={handelOnChange} />
+        <br/>
+        <button className='btn btn-primary' onClick={clickMe}>test</button>
+        <br/><br/>
+        <h3>{text}</h3>
+    </div>
     </>
   )
 }
-
-Navbar.propTypes = {
-  title: PropTypes.string,
-  home: PropTypes.string.isRequired,
-  reactLink: PropTypes.string.isRequired
-}
-
-Navbar.defaultProps  = {
-  title: "duf title",
-  home: "def home"
-} 
