b254ee1fd34d098274b5deb48f9b5c522c79fa5d
chetansankpal7007@gmail.com <chetansankpal7007@gmail.com>
2023-07-06 08:45:03 +0530

    jun_06_use_state_style_change

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,29 +2,58 @@ import React, {useState} from 'react'
 
 export default function Navbar(props) {
 
-  const [text, setText] = useState('demo text');
+  const [text, setText] = useState('demo');
+  const [showText, setShowText] = useState('');  
+  const [mystyle, setStyle] = useState({
+    color: 'black',
+    backgroundColor: 'white'
+
+  });
 
   const clickMe = () => {
-    console.log("click function call");
-    console.log(text);
-    setText("text change")
+    setShowText(text);
   }
 
-  const handelOnChange = (events) => {
-    console.log(events.target.value);
-    setText(events.target.value);
+  const textChange = (event) => {
+    let textValue = event.target.value;
+    setText(textValue);
   }
 
+  const changeStyle = () => {
+    if(mystyle.color === 'white') {
+      setStyle(
+        {
+          color: 'black',
+          backgroundColor: 'white'
+        }
+      )
+    } else {
+      setStyle(
+        {
+          color: 'white',
+          backgroundColor: 'black'
+        }
+      )
+    }
+    
+
+  }
 
   return (
     <>
-    <div className='contener m-5'>
-
-        <textarea value={text} onChange={handelOnChange} />
-        <br/>
-        <button className='btn btn-primary' onClick={clickMe}>test</button>
-        <br/><br/>
-        <h3>{text}</h3>
+    <div className='container m-5'>
+      <h1>Use State</h1>
+      <textarea rows='8' onChange={textChange}></textarea>
+      <br/>
+      <button className='btn btn-primary' onClick={clickMe}>button</button>
+      <div className="container" >
+        <h2 >Data</h2>
+        <p>{showText}</p>
+      </div>
+    </div>
+    <div className="container">
+      <h1 style={mystyle}>Hi this is React Class</h1>
+      <button className='btn btn-primary my-3' onClick={changeStyle}>Change style</button>
     </div>
     </>
   )
--- src/component/New_compoent.js
+++ src/component/New_compoent.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Newcompoent() {
+export default function New_compoent() {
   return (
     <div>
-      <h1>Hi this is  new component</h1>
+      
     </div>
   )
 }
