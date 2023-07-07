b57da0a51b893bda8352f24b03987e281add3d84
chetansankpal7007@gmail.com <chetansankpal7007@gmail.com>
2023-07-07 08:45:27 +0530

    jun-07-project-use-state-event

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import "./App_A.css";
 import Navbar from './component/Navbar'
-import Newcompoent from './component/New_compoent'
 const link= "https://react.dev/";
 function App() {
+
+  const [mode, setMode] = useState ('light');
+
+  const modeChange = () => {
+    if (mode === 'light') {
+      setMode("dark");
+      document.body.style.backgroundColor = '#35024f'
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = '#dccfe3'
+    }
+  }
   return (
     <>
-      <Navbar title="React Class -2"  reactLink={link}/>
-      <Navbar title="React Class -2"  reactLink={link}/>
-      <Navbar title="React Class -2"  reactLink={link}/>
-      <Newcompoent/>
+      <Navbar title="React Class -2" mode={mode} modeShwich={modeChange} reactLink={link}/>
     </>
   );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,37 +5,41 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          {props.title}
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} >
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">
-                {props.home} <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href={props.reactLink}>
-                Link
-              </a>
-            </li>
-          </ul>
-          <Search></Search>
-        </div>
+            <a className="navbar-brand" href="/">
+              {props.title}
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="/">
+                    {props.home} <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href={props.reactLink}>
+                    Link
+                  </a>
+                </li>
+              </ul>
+                <div className="form-check form-switch my-2 my-lg-0">
+                  <input className="form-check-input" onChange={props.modeShwich} type="checkbox" id="flexSwitchCheckDefault" />
+                  <label style={{color: props.mode === 'light' ? '#0d0c0c': '#f5f0f0'}} className="form-check-label" htmlFor="flexSwitchCheckDefault">Change Mode</label>
+                </div>
+            </div>
       </nav>
     </>
   )
