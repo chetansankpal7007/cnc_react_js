5901f5e919dbeca7c86f4f3c2fc155b6e7b97675
chetansankpal7007@gmail.com <chetansankpal7007@gmail.com>
2023-07-18 08:43:32 +0530

    jul-18-project-part-1

--- src/component/Table.js
+++ src/component/Table.js
@@ -1,39 +1,65 @@
 import React from 'react'
 import "./Table.css";
+import { useState } from 'react'
+const std = [
+    {nameItem: 'Chetan', conatctItem: '55555555'},
+    {nameItem: 'Ganesh', conatctItem: '666743456'},
+    {nameItem: 'Rahul', conatctItem: '456456456'},
+    {nameItem: 'mahesh', conatctItem: '4556767678'},
+    {nameItem: 'Rahul 2', conatctItem: '67867833'},
+]
 function StdTab (props)  {
-    // return <h2>{props.data.name } {props.data.contact}</h2>
-    return <tr >
-            <td >{props.data.name }</td>
-            <td>{props.data.contact}</td>    
+const deleteItem = (index) => {
+    std.splice(index, 1);
+    console.log(std);
+}
+
+return <tr >
+            <td >{props.data.nameItem }</td>
+            <td>{props.data.conatctItem}</td>  
+            <td><input type='button' value='Delete' onClick={deleteItem(props.itemIndex)}></input></td>  
             </tr>
 }
+
 export default function Table() {
-    // const std3 = {name: 'Chetan', contact: '99999999'}
-    // const std1 = {name: 'Mahesh', contact: '876699999'}
-    // const std2 = {name: 'Rahul', contact: '877777799'}
 
-    const std= [
-        {name: 'Chetan', contact: '99999999'},
-        {name: 'Mahesh', contact: '876699999'},
-        {name: 'Rahul', contact: '877777799'},
-        {name: 'Ganesh', contact: '38874774'}
-    ]
 
+ const [name, setName] = useState('');
+ const [contact, setContact] = useState('');
+//  const [std, setStd] = useState([{nameItem: 'Rahul', conatctItem: '877777799'}]);
+ 
+ 
+//  const save= () => {
+//     let tempStd= std;
+//     let obj= {nameItem: name, conatctItem: contact}
+//     tempStd.push(obj);
+//     setStd(tempStd);
+//     console.log(std);
+//  }
 
   return (
     <div className='m-5'>
+        <form >
+            <input type="text" id="name" onChange={(e)=> setName(e.target.value)} placeholder="Name"/><br/><br/>
+            <input type="text" id="contact" onChange={(e)=> setContact(e.target.value)} placeholder="Conatact No"/><br/><br/>
+            <input type='button' value='save'></input>
+        </form>
+
       <h1>Table Component</h1>
       <table>
         <tr>
             <td>
-                name
+                Name
             </td>
             <td>
                 Contact
             </td>
+            <td>
+                Delete
+            </td>
         </tr>
 
-         {std.map((stdItem) => <StdTab data={stdItem}></StdTab>)}
+         {std.map((stdItem, index) => <StdTab data={stdItem} itemIndex={index}></StdTab>)}
       </table>
     </div>
   )
